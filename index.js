c91af671fd6b76557d779b813b8eae05cdfba62c
Rachel Liu <rachel98rl@hotmail.com>
2018-01-02 21:11:28 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express"),
-  app = express(),
-  http = require("http").Server(app).listen(80);
-
-console.log("Server started at port 80")
-app.get("/", function(req, res) {
-  res.sendFile(__dirname+"/index.html")
-})
